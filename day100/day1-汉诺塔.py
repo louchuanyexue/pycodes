5ecfe4740032172213d516199199ee86eb74a65e
LouChuanYeXue <1520355655@qq.com>
2018-12-04 16:27:59 +0800

    again

---
+++ day100/day1-汉诺塔.py
@@ -0,0 +1,16 @@
+'''
+将 N 个圆盘从左边柱子移动到右边柱子：
+
+[递归的]将 N-1 个圆盘从左边柱子移动到中间柱子。
+将最大的圆盘从左边柱子移动到右边柱子。
+[递归的]将 N-1 个圆盘从中间柱子移动到右边柱子。
+'''
+def hano(height, left='left', right='right', midle='midle'):
+    if height:
+        hano(height-1,left,midle,right)
+        print(height,left ,"->", right)
+        hano(height-1,midle,right,left)
+
+hano(2)
+        
+    
